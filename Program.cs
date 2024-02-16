656fb0ea9e6bf13dbe4d34fd5ef68631e219272f
Kavyapuli <pulikavya10@gmail.com>
2024-02-16 10:13:16 +0530

    final commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+
+Console.WriteLine("Calculatio example");
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(6, 12);
